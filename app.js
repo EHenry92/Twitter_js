b4770c1ff957fb42456a8b23a2bff7bb78272bc9
Maria Betances <mbetances1002@gmail.com>
2017-09-19 10:30:48 -0400

    repo test

--- app.js
+++ app.js
@@ -5,7 +5,21 @@ app.listen(3000, function ()   {
     console.log('server listening on port 3000');
 });
 
-app.get('/ ', function (req, res) {
-    res.send('Welcome');
+app.use(function (req, res, next) {
+  console.log('Time:', Date.now())
+  next()
+});
 
+app.get('/',function (req, res, next) {
+    console.log('Is anybody there?');
+    next();
+});
+
+app.get('/', function (req, res, next){
+  res.send('Hi there!')
+});
+
+app.post('/', function (req, res){
+  console.log('test');
+  res.send('test');
 });
